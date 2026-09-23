95cb2f27726ab90e4845daddf7227fc0c3a77bca
matosjefferson-ui <matos.jefferson@escola.pr.gov.br>
2026-09-23 16:56:56 -0300

    concertei os erros do codido

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 cont botoesCurtir = document .querySelectorAll(".curtir");
 botoesCurtir.forEach(function(botaoCurtir){
-    let curtir = false;
+    let curtiu = false;
     botaoCurtir.addEventListener("click", curtir);
 function curtir(){
-    const contador - botaoCurtir.querySelector("span");
+    const contador = botaoCurtir.querySelector("span");
     if(curtiu === false){
         contador.textContent++;
         curtiu = true;}
